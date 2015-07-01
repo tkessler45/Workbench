fe49547e354ba3f74986c6c0baa2cb545634947c
Topher Kessler <tkessler@mac.com>
2015-06-30 20:32:04 -0700

    None

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import _thread
 #a = filescan('data.txt')
 #a.thescanner(a.thefile, a.subfirst)
 
-a = filefilter('infile.txt', filefilter.replacechars,"W","X")
+#a = filefilter('infile.txt', filefilter.replacechars,"W","X")
 #a.doit()
-_thread.start_new_thread(a.makeafile, ("/Users/tkessler/PycharmProjects/Workbench/filename.txt",))
+#_thread.start_new_thread(a.makeafile, ("/Users/tkessler/PycharmProjects/Workbench/filename.txt",))
 
 # b = filefilter()
 # b.thefile="infile.txt"
@@ -17,3 +17,14 @@ _thread.start_new_thread(a.makeafile, ("/Users/tkessler/PycharmProjects/Workbenc
 #b.thefile('infile.txt')
 
 
+def stage1():
+    tmp=1
+    def stage2():
+        def stage3():
+            print('stage3 %d' % tmp)
+        print('stage2 %d' % tmp)
+        stage3()
+    print('stage1 %d' % tmp)
+    stage2()
+
+stage1()
